cbb2eb47ca0a522df5ee9328feff78fbe177e36a
Javier <javiercorralgil@gmail.com>
2021-05-09 18:27:36 +0200

    update

--- lab-daemon.js
+++ lab-daemon.js
@@ -7,6 +7,10 @@ self.proxy_socket = null;
 var server = net.createServer(function(socket) {
     console.log("Server connected!");
     self.server_socket = socket;
+    self.server_socket.on('data', (data) => {
+        console.log("SERVER -> PROXY");
+        self.proxy_socket.write(data);
+    })
 });
 
 var proxy = net.createServer(function(socket) {
@@ -15,6 +19,8 @@ var proxy = net.createServer(function(socket) {
 
     proxy.on('data', function(data){
         console.log('Sending data through the tunnel');
+        console.log("PROXY -> SERVER");
+        self.server_socket.write(data);
     });
 
     proxy.on('end', function() {
@@ -23,7 +29,7 @@ var proxy = net.createServer(function(socket) {
 });
 
 // Tunnel
-server.listen(8041, '127.0.0.1');
+server.listen(8041, '192.168.18.2');
 
 // Proxy
 proxy.listen(4420, '127.0.0.1');
--- server-daemon.js
+++ server-daemon.js
@@ -1,9 +1,18 @@
 var net = require('net');
 
+var local = new net.Socket();
+
 var client = new net.Socket();
 client.connect(8041, '185.238.51.205', function() {
 	console.log('Connected');
-	
+	local.connect(8080, '127.0.0.1', function() {
+        console.log('Connected to local');
+    });
+
+    local.on('data', function(data){
+        client.write(data);
+    });
+
     //client.write('Hello, server! Love, Client.');
 });
 
